d548543deaa53724b39dd5962554d604b088933c
BhumiShriwas <166809984+BhumiShriwas@users.noreply.github.com>
2025-01-31 01:43:08 +0800

    Add files via upload

---
+++ UQ22.R
@@ -0,0 +1,6 @@
+
+sequence_2_to_8 <- seq(2, 8, length.out = 7)
+repeated_vector <- rep(c(3, -4.2, -50), times = 3)
+value_14_84_plus_3 <- 14 / 84 + 3
+final_vector <- c(sequence_2_to_8, repeated_vector, value_14_84_plus_3)
+final_vector
