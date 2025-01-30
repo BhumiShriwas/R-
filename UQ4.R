1dd9673c8b4db0f3010777c0b6f0825f03d0f167
BhumiShriwas <166809984+BhumiShriwas@users.noreply.github.com>
2025-01-31 00:35:23 +0800

    Update UQ4.R

--- UQ4.R
+++ UQ4.R
@@ -1,1 +1,9 @@
-vec4 <- c(6:12, rep(5.3, 3), -3, 102:(102 + 8))
+
+seq <- 6:12
+repeated_5_3 <- rep(5.3, 3)
+neg_3 <- -3
+length_of_vector_3 <- length(sorted_vector)
+seq_102_to_length <- seq(102, length.out = 9, by = (length_of_vector_3 - 102) / 8)
+final_vector <- c(seq, repeated_5_3, neg_3, seq_102_to_length)
+final_vector
+
