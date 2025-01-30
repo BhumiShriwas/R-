bc8499720c58429215d386dee4174a5e2ee3265c
BhumiShriwas <166809984+BhumiShriwas@users.noreply.github.com>
2025-01-31 01:37:07 +0800

    Add files via upload

---
+++ UQ20.R
@@ -0,0 +1,8 @@
+
+sequence_15 <- 15:25
+repeated_4_2 <- rep(4.2, 2)
+neg_5 <- -5
+length_question_2 <- length(reverse)
+sequence_200_to_length <- seq(200, length.out = 12, by = (length_question_2 - 200) / 11)
+final_vector <- c(sequence_15, repeated_4_2, neg_5, sequence_200_to_length)
+final_vector
