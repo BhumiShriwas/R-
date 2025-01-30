0089671b3722d76a54bcb417aeebecfaef66b13f
BhumiShriwas <166809984+BhumiShriwas@users.noreply.github.com>
2025-01-31 01:46:32 +0800

    Add files via upload

---
+++ UQ23.R
@@ -0,0 +1,7 @@
+
+final_vector <- c(seq(2, 8, length.out = 7), rep(c(3, -4.2, -50), times = 3), 14 / 84 + 3)
+new_object <- final_vector[c(1, 3)]
+new_object 
+combined_vector <- c(new_object, final_vector)
+combined_vector
+
