2a98ac48e6d4bb88bec7e6c8c72d3cd04829a628
BhumiShriwas <166809984+BhumiShriwas@users.noreply.github.com>
2025-01-31 01:49:29 +0800

    Add files via upload

---
+++ UQ24.R
@@ -0,0 +1,5 @@
+
+final_vector <- c(seq(2, 8, length.out = 7), rep(c(3, -4.2, -50), times = 3), 14 / 84 + 3)
+middle_indices <- 5:7
+final_vector[middle_indices] <- c(-0.5, -200, -0.5)
+final_vector
